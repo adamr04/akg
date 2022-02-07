a78b136220726c88b4f098926d94915fe26e93fc
zeitvertrieb <matthiasgrieder@gmail.com>
2022-02-07 17:40:42 +0100

    update gallery grid and h1

--- src/components/Headings/CalloutHeading/index.tsx
+++ src/components/Headings/CalloutHeading/index.tsx
@@ -4,7 +4,7 @@ import { HeadingsProps } from "@/definitions";
 export const CalloutHeading: React.FC<HeadingsProps> = ({ text }) => {
   return (
     <h1
-      className="h1 text-center pt-16 max-w-4xl mx-auto"
+      className="text-7xl md:text-8xl text-center pt-16 max-w-4xl mx-auto"
       data-aos="zoom-out"
       data-aos-delay="50"
       data-aos-duration="300"
